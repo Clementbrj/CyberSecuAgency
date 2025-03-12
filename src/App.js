a32da4e420e2e4d3f96ff252ff09f4496516a3a3
Clementbrj <153804717+Clementbrj@users.noreply.github.com>
2025-03-12 10:18:24 +0100

    deploy

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
-// Import des composants 
+// Import des composants
 import Acceuil from "./composants/acceuil";
 import Documentation from "./composants/documentation";
 import Expertise from "./composants/expertise";
 
 function App() {
   return (
-    <Router>
+    <Router basename="/CyberSecuAgency">
       <div>
         <Routes>
-        {/* Définition des routes */}
-          <Route path="/" element={<Acceuil/>}/> 
-          <Route path="/services" element={<Documentation/>}/>
-          <Route path="/expertise" element={<Expertise/>}/>
+          {/* Définition des routes */}
+          <Route path="/" element={<Acceuil />} />
+          <Route path="/services" element={<Documentation />} />
+          <Route path="/expertise" element={<Expertise />} />
         </Routes>
       </div>
     </Router>
